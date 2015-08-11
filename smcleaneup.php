092609e25ec6626b65f682d637708a328964b0bc
SimonJan <SimonJan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-11 17:17:24 +0000

    Update code 1.0
    git-svn-id: https://plugins.svn.wordpress.org/sm-cleanup/trunk@1218381 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ smcleaneup.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Plugin Name: SM Cleanup
+ * Plugin URI: #
+ * Version: 1.0
+ * Author: Simon Jan
+ * Author URI: https://profiles.wordpress.org/simonjan
+ * Description: A simple way optimize your web, clean your from code, convert style attributes to class attributes, use css external css instead of inline style, reuse your style
+ * License: GPLv3 or later
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'SMCL_V', '1.0' );
+
+define( 'SMCL_RQ_WP_V', '4.0' );
+
+define( 'SMCL_PLUGIN', __FILE__ );
+
+define( 'SMCL_PLUGIN_DIR', plugin_dir_path( ( SMCL_PLUGIN ) ) );
+
+define( 'SMCL_PLUGIN_URL', plugin_dir_url( SMCL_PLUGIN ) );
+
+define( 'SMCL_PLUGIN_ASSETS', SMCL_PLUGIN_URL . 'assets/');
+
+define( 'SMCL_ADMIN_URL', admin_url('admin.php?page=sm-cleanup') );
+
+/**
+ * Called when plugin active/deactive
+ */
+if( ! class_exists( 'SMCL_Activator ') ){
+	require_once( SMCL_PLUGIN_DIR .'class/class.smcl_activator.php' );
+	$smcl = new SMCL_Activator();
+	// // activator
+	register_activation_hook( SMCL_PLUGIN, array( &$smcl, 'smActivate') );
+
+	// deactivator
+	register_deactivation_hook( SMCL_PLUGIN, array( &$smcl, 'smDeActive') );
+}
+
+/**
+ * Define internationalization, dashboard-specific hooks, and public-facing site hooks.
+ */
+require_once( SMCL_PLUGIN_DIR . 'class/class.smcleanup.php');
